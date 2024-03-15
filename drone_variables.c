5997161e9667f39a1db3a43da3db44d2a413a0bd
tamiewong <tamiewongtwje@gmail.com>
2024-03-15 12:44:42 +0800

    Increase dist from wall
    even: 1.3m
    odd: 0.9m

--- drone_variables.c
+++ drone_variables.c
@@ -3,16 +3,16 @@
 #include "drone_variables.h"
 
 // WALL FOLLOWING: DRONE DIST FROM WALL VARIABLES
-float drone_dist_from_wall_1 = 0.6; // following on the left side of the wall
-float drone_dist_from_wall_2 = 1.2; // following on the right side of the wall
-float drone_dist_from_wall_corner_margin = 0.3; // error margin for turning around corner
-float drone_dist_from_wall_forward_margin = 0.1; // error margin for going forward along wall
+float drone_dist_from_wall_1 = 0.9; //0.6; // following on the left side of the wall
+float drone_dist_from_wall_2 = 1.3; //1.2; // following on the right side of the wall
+float drone_dist_from_wall_corner_margin = 0.2; //0.3; // error margin for turning around corner
+float drone_dist_from_wall_forward_margin = 0.05; //0.1; // error margin for going forward along wall
 float drone_dist_from_wall_to_start_margin = 0.2; // error margin for transitions (forward/corner)
 float drone_heading_threshold = 0.8; // in rad / Check if drone heading goes over 0.8 rad
 
 // WALL FOLLOWING: DRONE SPEED VARIABLES
-float drone_speed = 0.2; // default drone speed
-int drone_speed_corner_scale = 3; // scale down for turning around corner
+float drone_speed = 0.3; //0.2; default drone speed
+int drone_speed_corner_scale = 2; //3; // scale down for turning around corner
 int drone_speed_forward_adjust_scale = 2; // scale down for going forward along wall
 
 // RSSI COLLISION AVOIDANCE VARIABLES
